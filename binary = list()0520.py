9a695491c886ef05f2b647a6cad5536c98340a1d
Weeendy0822 <543987we@gmail.com>
2025-05-20 16:13:38 +0800

    Add files via upload

---
+++ binary = list()0520.py
@@ -0,0 +1,10 @@
+number = int(input('請輸入一個要轉換為二進位的數字:'))
+binary = list()
+while number > 1:
+    binary.append(str(number%2))
+    number//=2
+binary.append(str(number))
+print(''.join(reversed(binary)))
+
+
+
