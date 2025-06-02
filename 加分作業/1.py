ac6fe1274d649d43a393500e19b025e089c4fd13
Weeendy0822 <543987we@gmail.com>
2025-06-02 14:09:50 +0800

    Add files via upload

---
+++ 加分作業/1.py
@@ -0,0 +1,9 @@
+#整數格式化輸出(自行輸入 4 個數字)
+a=int(input('輸入1個數字:'))
+s=int(input('輸入1個數字:'))
+f=int(input('輸入1個數字:'))
+d=int(input('輸入1個數字:'))
+
+
+
+print(a,s,f,d)
---
+++ 加分作業/2.py
@@ -0,0 +1,9 @@
+#浮點數格式化輸出(自行輸入 4 個數字)
+a=float(input('輸入1個數字:'))
+s=float(input('輸入1個數字:'))
+f=float(input('輸入1個數字:'))
+d=float(input('輸入1個數字:'))
+
+
+
+print(a,s,f,d)
