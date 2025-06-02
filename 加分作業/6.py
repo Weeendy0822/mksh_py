865a1464eae63f62c4c0a19a631cdb458d4a72ac
Weeendy0822 <543987we@gmail.com>
2025-06-02 14:58:16 +0800

    Add files via upload

---
+++ 加分作業/6.py
@@ -0,0 +1,17 @@
+#分數顯示對應的等級
+f=int(input('輸入分數:'))
+if f >= 80 and f <= 100:
+    print("A")
+elif f >= 70 and f <= 79:
+    print("B")
+elif f >= 60 and f <= 69:
+    print("C")
+elif f <= 59:
+    print("F")
+
+
+
+
+
+
+
